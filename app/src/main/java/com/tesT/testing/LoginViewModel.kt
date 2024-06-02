e51f7bb5e186f987e8d8e5acb6e2c083480903d9
Visente Carvajal <carvajalvisente1999@gmail.com>
2024-06-02 17:01:14 -0400

    agregando el loginviewmodel

---
+++ app/src/main/java/com/tesT/testing/LoginViewModel.kt
@@ -0,0 +1,93 @@
+package com.tesT.testing
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.room.util.copy
+import com.google.firebase.auth.FirebaseAuth
+import com.tesT.testing.data.LoginUIEvent
+import com.tesT.testing.data.LoginUIState
+import com.tesT.testing.data.RegistrationUIState
+import com.tesT.testing.data.SignUpViewModel
+import com.tesT.testing.data.rules.Validator
+import com.tesT.testing.navigation.Screen
+import com.tesT.testing.navigation.trackFinderAppRouter
+
+class LoginViewModel : ViewModel(){
+
+    private val TAG = LoginViewModel::class.simpleName
+
+    var loginUIState = mutableStateOf(LoginUIState())
+    var allValidationsPassed = mutableStateOf(false)
+    var loginInProgress = mutableStateOf(false)
+
+
+    fun onEvent(event: LoginUIEvent){
+
+        when (event){
+            is LoginUIEvent.CorreoChanged -> {
+              loginUIState.value = loginUIState.value.copy(
+                  correo = event.correo
+              )
+
+            }
+            is LoginUIEvent.contraseñaChanged -> {
+                loginUIState.value = loginUIState.value.copy(
+                    contraseña = event.contraseña
+                )
+
+            }
+            is LoginUIEvent.LoginButtonClicked ->{
+                login()
+
+
+            }
+
+
+        }
+        validateLoginUIDataWithRules()
+    }
+
+
+    private fun validateLoginUIDataWithRules(){
+        val emailresult = Validator.validateCorreo(
+           fCorreo = loginUIState.value.correo
+        )
+
+        val passwordResult = Validator.validateContraseña(
+            fContraseña = loginUIState.value.contraseña
+        )
+
+        loginUIState.value =loginUIState.value.copy(
+            CorreoError = emailresult.status,
+            ContraseñaError = passwordResult.status
+        )
+
+        allValidationsPassed.value = emailresult.status && passwordResult.status
+    }
+
+    private fun login(){
+        loginInProgress.value = true
+        val correo = loginUIState.value.correo
+        val contraseña = loginUIState.value.contraseña
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(correo, contraseña)
+            .addOnCompleteListener{
+                loginInProgress.value = false
+                Log.d(TAG, "Inside_Login_Success")
+                Log.d(TAG, "login: ${it.isSuccessful}")
+
+                if(it.isSuccessful){
+                    loginInProgress.value = false
+                    trackFinderAppRouter.navigateTo(Screen.HomeScreen)
+
+                }
+            }
+            .addOnFailureListener{
+                Log.d(TAG, "Inside_Login_Failure")
+                Log.d(TAG, "login: ${it.localizedMessage}")
+                loginInProgress.value = false
+
+            }
+    }
+}
